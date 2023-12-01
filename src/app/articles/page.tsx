b920f43465d0f9cde540ae5e3c16a5dd429a7ce4
ChukwukaHonour <ChukwukaHonour@gmail.com>
2023-12-01 13:57:19 +0100

    Added the header and footer to all the pages

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,7 +1,13 @@
+import Footer from "@/components/Layouts/Footer";
+import Header from "@/components/Layouts/Header/Header";
 import React from "react";
 
+
 const Articles = () => {
-  return <div>Articles</div>;
+  return <main>
+    <Header />
+    <Footer />
+  </main>
 };
 
 export default Articles;
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,12 @@
+import Footer from "@/components/Layouts/Footer";
+import Header from "@/components/Layouts/Header/Header";
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return <main>
+    <Header />
+    <Footer />
+  </main>
 };
 
 export default Contact;
